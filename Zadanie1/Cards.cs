673aa1955941d22f28a089290e4272b965a8309d
Przemysław Skolimski <pskolimski@revku.dev>
2024-09-04 13:02:59 +0200

    initial commit

---
+++ Zadanie1/Cards.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace Zadanie1;
+
+public static class Cards
+{
+    public static List<string> CardsList { get; set; } = new List<string>();
+
+    static Cards()
+    {
+        LoadFromDirectory();
+    }
+
+    public static void LoadFromJson()
+    {
+        string jsonPath = "cards.json";
+
+        if (File.Exists(jsonPath))
+        {
+            string json = File.ReadAllText(jsonPath);
+            CardsList = JsonConvert.DeserializeObject<List<string>>(json);
+        }
+        else
+        {
+            MessageBox.Show("File not found");
+        }
+    }
+
+    public static void LoadFromDirectory()
+    {
+        string directoryPath = "images/cards";
+
+        if (Directory.Exists(directoryPath))
+        {
+            CardsList = Directory.GetFiles(directoryPath, "*.png").ToList();
+        }
+        else
+        {
+            MessageBox.Show("Directory not found");
+        }
+    }
+}
---
+++ Zadanie1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+///
+
+public class Card
+{
+    public string Color { get; set; }
+    public string Value { get; set; }
+}
+public partial class MainWindow : Window
+{
+    private List<Image> images;
+    private List<Card> actualCards = new List<Card>();
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        images = new List<Image>()
+        {
+            CardImg1,
+            CardImg2,
+            CardImg3,
+            CardImg4,
+            CardImg5
+        };
+    }
+
+    private void GenerateRandom(object sender, RoutedEventArgs e)
+    {
+        Random random = new Random();
+
+        List<Card> actualCards = new List<Card>();
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            int index = random.Next(Cards.CardsList.Count);
+            string cardPath = Cards.CardsList[index];
+            BitmapImage bitmap = new BitmapImage(new Uri(cardPath, UriKind.Relative));
+            images[i].Source = bitmap;
+
+            string[] cardInfo = cardPath.Split('_');
+            actualCards.Add(new Card()
+            {
+                Color = cardInfo[2],
+                Value = cardInfo[0]
+            });
+        }
+    }
+}
